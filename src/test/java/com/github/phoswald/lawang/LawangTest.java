ebacceacf185a84312b2f366f1f7ed3927460922
Philip Oswald <philip.oswald@gmail.com>
2018-02-18 21:20:39 +0100

    Evaluate another initializer API

--- src/test/java/com/github/phoswald/lawang/LawangTest.java
+++ src/test/java/com/github/phoswald/lawang/LawangTest.java
@@ -71,10 +71,24 @@ public class LawangTest {
     }
 
     @Test
+    public void builder4_withFields_correctValues() {
+        Person person = Lawang4.create(Person.class, (c, it) -> c
+            .set(it::name).to("Philip Oswald")
+            .set(it::email).to("philip.oswald@gmail.com")
+            .set(it::birthdate).to(LocalDate.of(1977, 10, 26))
+            .set(it::age).to(40));
+        assertNotNull(person);
+        assertEquals("Philip Oswald", person.name());
+        assertEquals("philip.oswald@gmail.com", person.email());
+        assertEquals(40, person.age());
+        assertEquals(LocalDate.of(1977, 10, 26), person.birthdate());
+    }
+
+    @Test
     public void builder_toString_byValue() {
         Person person = create(Person.class,
-                set(Person::name).to("Philip Oswald"),
-                set(Person::email).to("philip.oswald@gmail.com"));
+            set(Person::name).to("Philip Oswald"),
+            set(Person::email).to("philip.oswald@gmail.com"));
 
         assertEquals("Person {email=philip.oswald@gmail.com, name=Philip Oswald}", person.toString());
     }
@@ -82,13 +96,13 @@ public class LawangTest {
     @Test
     public void builder_equalsAndHashCode_byValue() {
         Person person = create(Person.class,
-                set(Person::name).to("Philip Oswald"),
-                set(Person::email).to("philip.oswald@gmail.com"));
+            set(Person::name).to("Philip Oswald"),
+            set(Person::email).to("philip.oswald@gmail.com"));
         Person person2 = create(Person.class,
-                set(Person::name).to("Philip Oswald"),
-                set(Person::email).to("philip.oswald@gmail.com"));
+            set(Person::name).to("Philip Oswald"),
+            set(Person::email).to("philip.oswald@gmail.com"));
         Person person3 = create(Person.class,
-                set(Person::name).to("Philip Oswald"));
+            set(Person::name).to("Philip Oswald"));
 
         assertTrue(person.equals(person2));
         assertEquals(person.hashCode(), person2.hashCode());
